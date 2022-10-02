ad92eb67f2528694108d9869c9cd7f903ced12d5
Pravin Singh Rana <dev.psrana@gmail.com>
2022-10-02 12:43:46 +0530

    File created: resources\views\inc\gallery.blade.php

---
+++ resources/views/inc/gallery.blade.php
@@ -0,0 +1,289 @@
+<section id="portfolio" class="portfolio">
+    <div class="container aos-init aos-animate" data-aos="fade-up">
+        <header class="section-header">
+            <h2>Gallery</h2>
+            <p>Check our latest pics</p>
+        </header>
+
+
+        <div class="row gy-4 portfolio-container aos-init aos-animate" data-aos="fade-up" data-aos-delay="200"
+            style="position: relative; height: 1008px;">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-app" style="position: absolute; left: 0px; top: 0px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-web" style="position: absolute; left: 440px; top: 0px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-app" style="position: absolute; left: 880px; top: 0px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-card"
+                style="position: absolute; left: 0px; top: 336px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-web"
+                style="position: absolute; left: 440px; top: 336px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-app"
+                style="position: absolute; left: 880px; top: 336px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-card"
+                style="position: absolute; left: 0px; top: 672px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-card"
+                style="position: absolute; left: 440px; top: 672px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-web"
+                style="position: absolute; left: 880px; top: 672px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item filter-web"
+                style="position: absolute; left: 880px; top: 672px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-10.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-10.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item filter-web"
+                style="position: absolute; left: 880px; top: 672px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-11.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-11.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item filter-web"
+                style="position: absolute; left: 880px; top: 672px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-12.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-12.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item filter-web"
+                style="position: absolute; left: 880px; top: 672px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-13.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-13.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item filter-web"
+                style="position: absolute; left: 880px; top: 672px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-14.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-14.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item filter-web"
+                style="position: absolute; left: 880px; top: 672px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-15.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-15.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item filter-web"
+                style="position: absolute; left: 880px; top: 672px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-16.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-16.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item filter-web"
+                style="position: absolute; left: 880px; top: 672px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-17.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-17.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item filter-web"
+                style="position: absolute; left: 880px; top: 672px;">
+                <div class="portfolio-wrap">
+                    <img src="assets/img/portfolio/portfolio-18.jpg" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Wall Street</h4>
+                        <p>Wall Street Fincap Services</p>
+                        <div class="portfolio-links">
+                            <a href="assets/img/portfolio/portfolio-18.jpg" data-gallery="portfolioGallery"
+                                class="portfokio-lightbox" title="Wall Street Image"><i class="bi bi-plus"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
