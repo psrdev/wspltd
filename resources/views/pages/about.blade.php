1b32a8f56abf4c0b79bf078e9bee71e0b8f5399d
Pravin Singh Rana <dev.psrana@gmail.com>
2022-10-02 12:43:46 +0530

    File created: resources\views\pages\about.blade.php

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.main')
+@section('main')
+<section class="contact">
+
+    <div class="container aos-init aos-animate" data-aos="fade-up">
+
+        <header class="section-header">
+            <h3>About</h3>
+
+        </header>
+
+        <div class="row gy-4">
+
+
+
+            <div class="col-lg-12">
+                <p>
+                    Wall Street Fincap Services Private Limited is a Private incorporated on 25 August 1994. It is classified as Non-govt
+                    company and is registered at Registrar of Companies, Delhi. Its authorized share capital is Rs. 25,000,000 and its paid
+                    up capital is Rs. 20,000,000. It is inolved in OTHER BUSINESS ACTIVITIES <br><br><br>
+
+                    Wall Street Fincap Services Private Limited's Annual General Meeting (AGM) was last held on 29 September 2020 and as per
+                    records from Ministry of Corporate Affairs (MCA), its balance sheet was last filed on 31 March 2020. <br><br><br>
+
+                    Directors of Wall Street Fincap Services Private Limited are Anil Kumar Sharma, Vijay Singh, Sanjay Singh.Manoj Sharma and Jaigovind Sharma <br><br><br>
+
+                    Wall Street Fincap Services Private Limited's Corporate Identification Number is (CIN) U74899DL1994PTC061100 and its
+                    registration number is 61100.Its Email address is anilsinghadvocate71@gmail.com and its registered address is A 61,
+                    Okhla Industrial Area Phase II New Delhi New Delhi DL 110020 IN
+                </p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+@endsection
