b9765cb3389fa86fb7554065f48f06850d4d83a5
Pravin Singh Rana <dev.psrana@gmail.com>
2022-10-02 12:43:46 +0530

    File created: resources\views\inc\services.blade.php

---
+++ resources/views/inc/services.blade.php
@@ -0,0 +1,43 @@
+<section id="services" class="values">
+
+    <div class="container aos-init aos-animate" data-aos="fade-up">
+
+        <header class="section-header">
+            {{-- <h2>Our Services</h2> --}}
+            <p>Our Services</p>
+        </header>
+
+        <div class="row">
+
+            <div class="col-lg-4">
+                <div class="box aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+                    <img src="assets/img/values-1.png" class="img-fluid" alt="">
+                    <h3>BUSINESS LOANS</h3>
+                    <p>we provide Business Loans to small businesses who want to grow and stay alive into the competitive market. If you have a
+                    established business for more than 10 months and in need of Financial Aid</p>
+                </div>
+            </div>
+
+            <div class="mt-4 col-lg-4 mt-lg-0">
+                <div class="box aos-init aos-animate" data-aos="fade-up" data-aos-delay="400">
+                    <img src="assets/img/values-3.png" class="img-fluid" alt="">
+                    <h3>MINIMUM DOCS</h3>
+                    <p>Getting a Business Loan at Wall Street Fincap Services Pvt Ltd is easy and requires minimum documents for applying for
+                    the Loan.</p>
+                </div>
+            </div>
+
+            <div class="mt-4 col-lg-4 mt-lg-0">
+                <div class="box aos-init aos-animate" data-aos="fade-up" data-aos-delay="600">
+                    <img src="assets/img/values-2.png" class="img-fluid" alt="">
+                    <h3>SAME DAY ISSUANCE</h3>
+                    <p>It is one of the services that separates us from other finance companies. Once your documents are completed and
+                    verified, the amount is disbursed on the same day</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
