1c54718a6c067e16c2fa0169236d673842a3afe1
Pravin Singh Rana <dev.psrana@gmail.com>
2022-10-02 12:43:46 +0530

    File created: app\Http\Controllers\BranchController.php

---
+++ app/Http/Controllers/BranchController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Branch;
+use Illuminate\Http\Request;
+
+class BranchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index( )
+    {
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+       return view('admin.branch.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+                $request->validate([
+
+            'main_image' => 'image|mimes:jpeg,png,jpg|max:2048',
+
+        ]);
+        Branch::create($request->all());
+
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Branch  $branch
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Branch $branch)
+    {
+
+          $branch2= Branch::where('id',$branch->id)->get();
+
+    //   return $branch2;
+
+        return view('pages.branch')->with('branch',$branch2);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Branch  $branch
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Branch $branch)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Branch  $branch
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Branch $branch)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Branch  $branch
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Branch $branch)
+    {
+        //
+    }
+}
