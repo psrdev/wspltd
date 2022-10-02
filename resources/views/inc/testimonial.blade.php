b41107f65dd0716d58055d1d178243a39fabeb96
Pravin Singh Rana <dev.psrana@gmail.com>
2022-10-02 12:43:46 +0530

    File created: resources\views\inc\testimonial.blade.php

---
+++ resources/views/inc/testimonial.blade.php
@@ -0,0 +1,123 @@
+<section id="testimonials" class="testimonials">
+
+    <div class="container aos-init aos-animate" data-aos="fade-up">
+
+        <header class="section-header">
+            <h2>Testimonials</h2>
+            <p>What they are saying about us</p>
+        </header>
+
+        <div class="testimonials-slider swiper swiper-initialized swiper-horizontal swiper-pointer-events aos-init aos-animate"
+            data-aos="fade-up" data-aos-delay="200">
+            <div class="swiper-wrapper" style="transform: translate3d(-3024px, 0px, 0px); transition-duration: 0ms;"
+                id="swiper-wrapper-8104b04f288b2e981" aria-live="off">
+                <div class="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="2" style="width: 432px;"
+                    role="group" aria-label="3 / 5">
+                    <div class="testimonial-item">
+                        <div class="stars">
+                            <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                class="bi bi-star-fill"></i>
+                        </div>
+                        <p>
+                            We Appreciate and thanks for your support,Thank You
+                        </p>
+                        <div class="mt-auto profile">
+                            {{-- <img src="assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt=""> --}}
+                            <h3>Shreyash Singh</h3>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-prev" data-swiper-slide-index="3"
+                    style="width: 432px;" role="group" aria-label="4 / 5">
+                    <div class="testimonial-item">
+                        <div class="stars">
+                            <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                class="bi bi-star-fill"></i>
+                        </div>
+                        <p>
+                            Very good service and policies,Satisfied
+                        </p>
+                        <div class="mt-auto profile">
+                            {{-- <img src="assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt=""> --}}
+                            <h3>Raunit Lohani</h3>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active"
+                    data-swiper-slide-index="4" style="width: 432px;" role="group" aria-label="5 / 5">
+                    <div class="testimonial-item">
+                        <div class="stars">
+                            <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                class="bi bi-star-fill"></i>
+                        </div>
+                        <p>
+                            Very well organized and easy business loan. Any business development need will be fulfilled here just in mere movements...
+                        </p>
+                        <div class="mt-auto profile">
+                            {{-- <img src="assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt=""> --}}
+                            <h3>Abhishek Sharma</h3>
+                            {{-- <h4>Entrepreneur</h4> --}}
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide swiper-slide-duplicate-next" data-swiper-slide-index="0" style="width: 432px;"
+                    role="group" aria-label="1 / 5">
+                    <div class="testimonial-item">
+                        <div class="stars">
+                            <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                class="bi bi-star-fill"></i>
+                        </div>
+                        <p>
+                           Hassel free with minimum documention and quick approvel of loan.Highly recommended for business purposes.
+                        </p>
+                        <div class="mt-auto profile">
+                            {{-- <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt=""> --}}
+                            <h3>Sumit Singh</h3>
+                            {{-- <h4>Ceo &amp; Founder</h4> --}}
+                        </div>
+                    </div>
+                </div><!-- End testimonial item -->
+
+                <div class="swiper-slide" data-swiper-slide-index="1" style="width: 432px;" role="group"
+                    aria-label="2 / 5">
+                    <div class="testimonial-item">
+                        <div class="stars">
+                            <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
+                                class="bi bi-star-fill"></i>
+                        </div>
+                        <p>
+                            Best Business Loan Agency In India. Very easy disbursment of loan and easy instalment .
+                        </p>
+                        <div class="mt-auto profile">
+                            {{-- <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt=""> --}}
+                            <h3>Pawan Sharma</h3>
+                            {{-- <h4>Designer</h4> --}}
+                        </div>
+                    </div>
+                </div><!-- End testimonial item -->
+
+
+            </div>
+            <div
+                class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal">
+                <span class="swiper-pagination-bullet" tabindex="0" role="button"
+                    aria-label="Go to slide 1"></span><span class="swiper-pagination-bullet" tabindex="0" role="button"
+                    aria-label="Go to slide 2"></span><span class="swiper-pagination-bullet" tabindex="0" role="button"
+                    aria-label="Go to slide 3"></span><span class="swiper-pagination-bullet" tabindex="0" role="button"
+                    aria-label="Go to slide 4"></span><span
+                    class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button"
+                    aria-label="Go to slide 5"></span></div>
+            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+        </div>
+
+    </div>
+
+</section>
