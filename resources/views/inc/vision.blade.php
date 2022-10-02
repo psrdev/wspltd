6280179ad36440a993b78cae0b7d4328c874395f
Pravin Singh Rana <dev.psrana@gmail.com>
2022-10-02 12:43:46 +0530

    File created: resources\views\inc\vision.blade.php

---
+++ resources/views/inc/vision.blade.php
@@ -0,0 +1,32 @@
+<!-- ======= About Section ======= -->
+<section id="vision" class="about">
+
+    <div class="container" data-aos="fade-up">
+        <div class="row gx-0">
+
+            <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
+                <div class="content">
+                    {{-- <h3>Our Vision</h3> --}}
+                    <h2>Our Vision</h2>
+                    <p>
+                        Our Vision is to always keep our business family's tremendous growth.We create trustfull and
+                        transparent bond.
+                    </p>
+                    {{-- <div class="text-center text-lg-start">
+                        <a href="#"
+                            class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
+                            <span>Read More</span>
+                            <i class="bi bi-arrow-right"></i>
+                        </a>
+                    </div> --}}
+                </div>
+            </div>
+
+            <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+                <img src="assets/img/vision.jpg" class="img-fluid" alt="Vision">
+            </div>
+
+        </div>
+    </div>
+
+</section><!-- End About Section -->
