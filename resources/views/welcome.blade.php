a383b599c1be6a91d74a3b2c351f999fdde85218
Pravin Singh Rana <dev.psrana@gmail.com>
2022-10-02 12:43:46 +0530

    File created: resources\views\welcome.blade.php

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+@section('main')
+
+@include('inc.hero')
+@include('inc.vision')
+@include('inc.about')
+@include('inc.counter')
+@include('inc.services')
+@include('inc.branches')
+
+@include('inc.testimonial-2')
+@include('inc.contact')
+
+
+@endsection
