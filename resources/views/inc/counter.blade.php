1af117c7cf29a62d566fdc24b2b169513e88dd81
Pravin Singh Rana <dev.psrana@gmail.com>
2022-10-02 12:43:46 +0530

    File created: resources\views\inc\counter.blade.php

---
+++ resources/views/inc/counter.blade.php
@@ -0,0 +1,53 @@
+<section id="counts" class="counts">
+    <div class="container aos-init aos-animate" data-aos="fade-up">
+
+        <div class="row gy-4">
+
+            <div class="col-lg-3 col-md-6">
+                <div class="count-box">
+                    <i class="bi bi-emoji-smile"></i>
+                    <div>
+                        <span data-purecounter-start="0" data-purecounter-end="932" data-purecounter-duration="1"
+                            class="purecounter">932</span>
+                        <p>Happy Clients</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="count-box">
+                    <i class="bi bi-journal-richtext" style="color: #ee6c20;"></i>
+                    <div>
+                        <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1"
+                            class="purecounter">521</span>
+                        <p>Projects</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="count-box">
+                    <i class="bi bi-headset" style="color: #15be56;"></i>
+                    <div>
+                        <span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1"
+                            class="purecounter">1463</span>
+                        <p>Hours Of Support</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="count-box">
+                    <i class="bi bi-people" style="color: #bb0852;"></i>
+                    <div>
+                        <span data-purecounter-start="0" data-purecounter-end="150" data-purecounter-duration="1"
+                            class="purecounter">150</span>
+                        <p>Hard Workers</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section>
