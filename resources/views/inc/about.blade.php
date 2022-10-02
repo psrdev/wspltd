9c4bf19e6025a5a509c1e066356acbf3e066b858
Pravin Singh Rana <dev.psrana@gmail.com>
2022-10-02 12:43:46 +0530

    File created: resources\views\inc\about.blade.php

---
+++ resources/views/inc/about.blade.php
@@ -0,0 +1,33 @@
+<section id="about" class="about">
+
+    <div class="container" data-aos="fade-up">
+        <div class="row gx-0">
+            {{-- <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+                <img src="assets/img/about.jpg" class="img-fluid" alt="About">
+            </div> --}}
+
+            <div class="col-lg-12 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
+                <div class="content">
+                    {{-- <h3>Our Vision</h3> --}}
+                    <h2 class="text-center">About</h2>
+                    <p>
+                      Located in Gorakhpur driven by ANIL KUMAR SHARMA, SANJAY SINGH and VIJAY
+                    SINGH, MANOJ SHARMA and JAIGOVIND SHARMA Incorporated on Aug 25, 1994. The registration number is 061100 and its Corporate Identification Number (CIN) is
+                    U74899DL1994PTC061100.
+                    </p>
+                    <div class="text-center text-lg-start">
+                        <a href="/about"
+                            class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
+                            <span>Read More</span>
+                            <i class="bi bi-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+
+</section>
