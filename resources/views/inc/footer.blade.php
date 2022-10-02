1ea70e435edfb10d8f7a68ff77fdd5e590396346
Pravin Singh Rana <dev.psrana@gmail.com>
2022-10-02 12:43:46 +0530

    File created: resources\views\inc\footer.blade.php

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,88 @@
+<footer id="footer" class="footer">
+
+    {{-- <div class="footer-newsletter">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="text-center col-lg-12">
+                    <h4>Our Newsletter</h4>
+                    <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                </div>
+                <div class="col-lg-6">
+                    <form action="" method="post">
+                        <input type="email" name="email"><input type="submit" value="Subscribe">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+
+    <div class="footer-top">
+        <div class="container">
+            <div class="row gy-4">
+                <div class="col-lg-5 col-md-12 footer-info">
+                    <a href="/" class="logo d-flex align-items-center">
+                        {{-- <img src="assets/img/logo.png" alt=""> --}}
+                        <span>WSFS</span>
+                    </a>
+                    <p>Located in Gorakhpur driven by ANIL KUMAR SHARMA, SANJAY SINGH and VIJAY
+                    SINGH.Incorporated on Aug 25, 1994. The registration number is 061100 and its Corporate Identification Number (CIN) is
+                    U74899DL1994PTC061100.</p>
+                    <div class="mt-3 social-links">
+                        <!--<a href="#" class="twitter"><i class="bi bi-twitter"></i></a>-->
+                        <a href="https://www.facebook.com/Wall-street-fincap-services-pvt-ltd-101056462280953/" class="facebook"><i class="bi bi-facebook"></i></a>
+                        <a href="http://instagram.com/wallstreetfincapservices" class="instagram"><i class="bi bi-instagram"></i></a>
+                         <a href="https://twitter.com/wallstreetpld?t=u3Xp5R_X6oxaD5nbzRBkNA&s=09" class="instagram"><i class="bi bi-twitter"></i></a>
+                        <a href="https://www.linkedin.com/in/wall-street-fincap-services-pvt-ltd-1ba74b22a" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><i class="bi bi-chevron-right"></i> <a href="/">Home</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="/about">About us</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-2 col-6 footer-links">
+                    <h4>Our Services</h4>
+                    <ul>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#services">Services</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#services">Business Loan</a></li>
+
+                    </ul>
+                </div>
+
+                <div class="text-center col-lg-3 col-md-12 footer-contact text-md-start">
+                    <h4>Contact Us</h4>
+                    <p>
+                        HIG-42 Shastripuram, <br>
+                        Rajendra nagar, Gorakhnath<br>
+                        Gorakhpur, Uttar Pradesh 273015 <br><br>
+                        <strong>Phone:</strong> +91 9455007668<br>
+                        <strong>Email:</strong> info@wallstreetpvtltd.com<br>
+                    </p>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="copyright">
+            © Copyright <strong><span>WSFS</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/flexstart-bootstrap-startup-template/ -->
+            Designed by <a href="https://psrdev.com">Pravin Singh Rana</a>
+        </div>
+    </div>
+</footer>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center active"><i
+        class="bi bi-arrow-up-short"></i></a>
